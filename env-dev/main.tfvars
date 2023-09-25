04d24338cd13e00d08601807e2fcfcd0f4918e1e
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-09-25 22:24:58 +0530

    adding the aws ssm parameter store

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,2 +1,4 @@
-instance_type = "t2.micro"
-env = "dev"
+parameter [
+    { name: "dev.frontend.catalogue_url", value = "http://catalogue-dev.rdevops72.online:8080/"},
+    { name: "dev.frontend.cart_url", value = "http://cart-dev.rdevops72.online:8080/"}
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "SecureString"
-  value = "Hello World"
+resource "aws_ssm_parameter" "parameters" {
+  count = lenght(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
   key_id = "469bca55-9725-4bf1-b84a-cb18f5c2809b"
 }
